87700d4f7325bd72e3384a0b28d4c9bdbb1b2b10
Taylor Bayless <taylorbayless314@gmail.com>
2024-12-01 22:24:05 -0600

    Add ability for user to submit text.

--- src/app/word-block/word-block.component.ts
+++ src/app/word-block/word-block.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-word-block',
   templateUrl: './word-block.component.html',
   styleUrls: ['./word-block.component.css'],
-  imports: [CommonModule]
+  standalone: true,
+  imports: [CommonModule, FormsModule]
 })
 export class WordBlockComponent {
-  text = 'The quick brown fox jumps over the lazy dog';
-  words = this.text.split(' ').map(word => ({
-    text: word,
-    isHidden: true
-  }));
+  inputText = ''; 
+  words: { text: string; isHidden: boolean }[] = [];
+
+  submitText(event: Event): void {
+    event.preventDefault();
+    this.words = this.inputText.split(' ').map(word => ({
+      text: word,
+      isHidden: false
+    }));
+  }
 
   toggleWord(index: number): void {
     if (this.words[index].isHidden) {
