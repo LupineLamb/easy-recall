1ba7e1ffb897dd1b2ec87231e6d57cb34ce1d30a
Taylor Bayless <taylorbayless314@gmail.com>
2024-12-19 12:36:28 -0600

    Remove unused Router import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WordBlockComponent } from './word-block/word-block.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WordBlockComponent],
+  imports: [WordBlockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
